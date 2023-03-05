2f06df2e3e30567c8afcaa505beeac98319bfe75
Stiliyan Dyankov <stiliandiankov0704@gmail.com>
2023-03-05 18:19:56 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+		<div className="App">
+			haiaia
+			<p className="font-bold text-red-500 underline">fkjfdk</p>
+			<Button variant="contained" className="bg-black">Hello</Button>
+		</div>
+	);
+}
+
+export default App;
